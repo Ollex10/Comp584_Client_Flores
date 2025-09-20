767b49b05e57edd41c8c5dfe3d759f9497f3eef0
ollex10 <flores.isai66@gmail.com>
2025-09-19 18:38:15 -0700

    Stuff we did in class today

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,43 +1,16 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
 
-import { Navbar } from './components/navbar/navbar';
+import { NavBar } from './components/navbar/navbar';
 
-import { Api, WeatherForecast } from './services/api';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, Navbar],
+  imports: [RouterOutlet, NavBar],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
-export class App implements OnInit {
-  protected readonly title = signal('Comp584_Client_Flores');
-
-  loading = false;
-  error: string | null = null;
-  forecasts: WeatherForecast[] = [];  // store WeatherForecast data
-
-  constructor(private api: Api) {}
-
-  ngOnInit(): void {
-    this.loadWeatherForecast();
-  }
-
-  loadWeatherForecast(): void {
-    this.loading = true;
-    this.api.getWeatherForecast().subscribe({
-      next: (data: WeatherForecast[]) => {
-        this.forecasts = data;
-        this.loading = false;
-      },
-      error: (err: any) => {
-        console.error('API error:', err);
-        this.error = err?.message ?? 'Unknown error';
-        this.loading = false;
-      }
-    });
-  }
+export class App {
+  protected readonly title = signal('COMP584_Client_Flores');
 }
